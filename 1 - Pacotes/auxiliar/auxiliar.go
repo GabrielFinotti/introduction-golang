3286bd68169f646f1a955eacfb7599bfaee76d59
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-03-13 21:27:39 -0300

    Pacotes
    Entendendo o uso dos pacotes

---
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,9 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever é uma função que escreve no terminal
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar")
+	escrever2()
+}
---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever2 é uma função que escreve no terminal
+func escrever2() {
+	fmt.Println("Escrevendo do pacote auxiliar2")
+}
