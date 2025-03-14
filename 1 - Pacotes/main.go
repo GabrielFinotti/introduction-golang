ec62800c9b39ab6bfd0e0e37ad0343b635b26ac4
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-03-13 21:33:21 -0300

    Utilizando pacotes externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/badoux/checkmail"
 	"modulo/auxiliar"
 )
 
 func main() {
 	fmt.Println("Olá, mundo!")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("finotti.dev@gmail.com")
+	fmt.Println(erro)
+
+	erro2 := checkmail.ValidateFormat("finotti.dev")
+	fmt.Println(erro2)
 }
